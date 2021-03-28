7143c5ed112d7f822ef80fbc8785a9b579145e82
aidan dodds <aidan.dodds@googlemail.com>
2021-03-28 14:29:04 +0100

    Move SmallVector inside of BVH

--- src/BVH.cpp
+++ src/BVH.cpp
@@ -1,7 +1,6 @@
 #include <assert.h>
 
 #include "bvh.h"
-#include "SmallVector.h"
 
 // enable to validate the tree after every operation
 #define VALIDATE 0
@@ -129,6 +128,59 @@ protected:
     assert(!(branch & ~1u));
     return index * 2 + branch;
   }
+};
+
+template <typename type_t, size_t default_size>
+struct small_vector_t {
+
+  small_vector_t()
+    : head(0)
+  {
+  }
+
+  size_t size() const {
+    return head;
+  }
+
+  bool empty() const {
+    return head == 0;
+  }
+
+  void push_back(const type_t &t) {
+    assert(head < data.size());
+    data[head++] = t;
+  }
+
+  type_t &front() {
+    assert(!empty());
+    return data[0];
+  }
+
+  type_t &back() {
+    assert(!empty());
+    return data[head - 1];
+  }
+
+  type_t *begin() const {
+    return &data[0];
+  }
+
+  type_t *end() const {
+    return &data[head];
+  }
+
+  void clear() {
+    head = 0;
+  }
+
+  void pop() {
+    assert(!empty());
+    --head;
+  }
+
+protected:
+  size_t head;
+  std::array<type_t, default_size> data;
 };
 
 }  // namespace {}
